2403e5f673e96fb369b15ff92d2805753215754a
Reinaldo Stephens <reinaldo9519@gmail.com>
2024-04-26 00:03:29 -0600

    agregando datos

--- index.php
+++ index.php
@@ -7,21 +7,38 @@ $ch = curl_init(API_URL);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 /* Ejecutamos la petición y guardamos el resultado */
 $result = curl_exec($ch);
+// una alternativa seria utilizar file_get_contents para hacer un GET a un API
 
 if ($e = curl_error($ch)) {
     echo $e;
-} else {
-    $data = json_decode($result, true);
-    var_dump($data);
 }
 
+$data = json_decode($result, true);
 curl_close($ch);
 
 ?>
 
+<head>
+    <meta name="description" content="Testing">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="X-UA-Compatible" content="IE=7">
+    <meta name="keywords" content="">
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.classless.min.css">
+</head>
+
 
 <main>
+    <section>
+        <img src="<?= $data["poster_url"]; ?>" width="200" alt="Poster de <?= $data["title"]; ?>">
+    </section>
 
+    <hgroup>
+        <h3><?= $data["title"]; ?> se estrena en <?= $data["days_until"]; ?></h3>
+        <p>Fecha de estreno: <?= $data["release_date"]; ?></p>
+        <p>La siguiente es <?= $data["following_production"]["title"]; ?></p>
+    </hgroup>
 </main>
 
 <style>
@@ -32,5 +49,23 @@ curl_close($ch);
     body {
         display: grid;
         place-content: center;
+    }
+
+    img {
+        border-radius: 16px;
+        margin: 0 auto;
+    }
+
+    section {
+        display: flex;
+        justify-content: center;
+        text-align: center;
+    }
+
+    hgroup {
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+        text-align: center;
     }
 </style>
